4fdf72c5d160421d0e89c419520e1f8ede99b43a
Matthew Blackledge <mattyblackledge@gmail.com>
2024-01-21 22:38:56 +0000

    Added improved script for building libraries

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# Define the name of the temporary conda environment and grpc version
+env_name="temp_env"
+grpc_version="1.55.0"
+
+# Create the temporary conda environment
+conda create --name "$env_name" python=3.8
+
+# Activate the environment
+source activate "$env_name"
+
 # Remove the current grpc clone
 # =============================
 if [ -d grpc ]; then
@@ -8,7 +18,7 @@ fi
 
 # Clone grpc
 # ==========
-git clone --recurse-submodules -b v1.50.0 https://github.com/grpc/grpc
+git clone --recurse-submodules -b v"$grpc_version" https://github.com/grpc/grpc
 
 pushd grpc
 
@@ -26,8 +36,8 @@ make -j 4
 make install
 popd
 
-# Build/install arm64 (M1) grpc libraries
-# ========================================
+# Build/install arm64 grpc libraries
+# ==================================
 mkdir -p cmake/build-arm64
 mkdir -p install-arm64
 pushd cmake/build-arm64
@@ -36,24 +46,9 @@ cmake -DgRPC_INSTALL=ON \
       -DCMAKE_INSTALL_PREFIX=../../install-arm64 \
       -DCMAKE_OSX_ARCHITECTURES=arm64\
       ../..
-make -j 4
+make -j 1
 make install
 popd
-
-# TODO: Build for arm64e currently not working.
-## Build/install arm64e (M2) grpc libraries
-## ========================================
-#mkdir -p cmake/build-arm64e
-#mkdir -p install-arm64e
-#pushd cmake/build-arm64e
-#cmake -DgRPC_INSTALL=ON \
-#      -DgRPC_BUILD_TESTS=OFF \
-#      -DCMAKE_INSTALL_PREFIX=../../install-arm64e \
-#      -DCMAKE_OSX_ARCHITECTURES=arm64e\
-#      ../..
-#make -j 4
-#make install
-#popd
 
 # Create universal libraries
 # ==========================
@@ -76,25 +71,41 @@ for file1 in install-arm64/lib/*.a; do
     fi
 done
 
-# Copy the relevant binaries/headers
-# ==================================
+# Copy the relevant binaries/headers for protocol compilation
+# ===========================================================
 machine_arch=$(arch)
 cp -r install-$machine_arch/bin install
 cp -r install-$machine_arch/include install
 
 popd
 
-# Build the protocol files
-# ========================
-mkdir -p cpp
-mkdir -p python
-
 # Build the cpp definitions
+# =========================
+mkdir -p cpp
 grpc/install/bin/protoc -I protos --grpc_out=cpp --plugin=protoc-gen-grpc=grpc/install/bin/grpc_cpp_plugin osirix.proto
-
 grpc/install/bin/protoc -I protos --cpp_out=cpp osirix.proto roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
+
+# Build the Python definitions
+# ============================
+mkdir -p python/grpc
+
+# This fixes issues with grpc version 1.55.0 on M1 (see https://stackoverflow.com/questions/72620996/apple-m1-symbol-not-found-cfrelease-while-running-python-app)
+export GRPC_PYTHON_LDFLAGS=" -framework CoreFoundation"
+
+# Install packages or perform other actions in the environment
+pip install grpcio=="$grpc_version" grpcio-tools=="$grpc_version" --no-binary :all:
 
 # Build the Python definitions
 python -m grpc_tools.protoc -I protos --python_out=python/osirixgrpc --grpc_python_out=python/osirixgrpc osirix.proto
 
 python -m grpc_tools.protoc -I protos --python_out=python/osirixgrpc roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
+
+# Deactivate the environment when done
+conda deactivate
+
+# Remove the temporary environment (optional)
+conda env remove --name "$env_name" --yes
+
+# Remind user to re-import libraries in XCode
+# ===========================================
+echo "Finished installation! Don't forget to re-link the compiled libraries (in grpc/install/lib) with the 'Build Phases' stage of XCode project."
