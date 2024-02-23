87e54a177ffaed073665670b904bee7cf3a95227
Matthew Blackledge <mattyblackledge@gmail.com>
2024-02-23 10:40:26 +0000

    bumpversion

--- pyosirix/osirix/__init__.py
+++ pyosirix/osirix/__init__.py
@@ -13,7 +13,7 @@ __all__ = ["Osirix",
            "GrpcException",
            "OsirixServiceException"]
 
-__version__ = "0.2.1-dev2"
+__version__ = "0.2.1-dev3"
 
 __author__ = "Timothy Sum Hon Mun & Matthew D Blackledge"
 
