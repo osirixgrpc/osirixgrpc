406c4d9e10ff63a8fe02b0cde4e15c43a23bbcfe
Matthew Blackledge <mattyblackledge@gmail.com>
2024-02-29 13:50:55 +0000

    Fixed version in README

--- python/osirixgrpc/__init__.py
+++ python/osirixgrpc/__init__.py
@@ -12,7 +12,7 @@ __all__ = ["browsercontroller_pb2",
            "viewercontroller_pb2",
            "vrcontroller_pb2"]
 
-__version__ = "0.2.1-dev8"
+__version__ = "0.2.1-dev9"
 
 __author__ = "Timothy Sum Hon Mun & Matthew D Blackledge"
 
