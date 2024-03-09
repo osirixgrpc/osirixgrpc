9b244891f77a63d3b176183f8a01b056b7608d47
Matthew Blackledge <mattyblackledge@gmail.com>
2024-03-08 21:19:32 -0600

    Added dicom series tests

--- tests/integration_tests_osirixgrpc/run_tests.sh
+++ tests/integration_tests_osirixgrpc/run_tests.sh
@@ -25,6 +25,7 @@ pip install -i https://pypi.org/simple/ --extra-index-url https://test.pypi.org/
 # Run pytest on the entire test directory: -s flag ensures print statements are available; -x stops after first fail.
 pytest test_browser_controller.py -s -x
 pytest test_dicom_image.py -s -x
+pytest test_dicom_series.py -s -x
 
 # Deactivate the virtual environment
 deactivate
