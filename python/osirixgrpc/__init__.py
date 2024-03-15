984036f1b1064f511143abfff17755f1812ba929
Matthew Blackledge <mattyblackledge@gmail.com>
2024-03-15 10:19:06 -0500

    bump dev37

--- python/osirixgrpc/__init__.py
+++ python/osirixgrpc/__init__.py
@@ -12,7 +12,7 @@ __all__ = ["browsercontroller_pb2",
            "viewercontroller_pb2",
            "vrcontroller_pb2"]
 
-__version__ = "0.2.1-dev36"
+__version__ = "0.2.1-dev37"
 
 __author__ = "Timothy Sum Hon Mun & Matthew D Blackledge"
 
