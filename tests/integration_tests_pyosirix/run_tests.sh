c5af8fb52763a63cac4984132b5154ebcb1fa634
Matthew Blackledge <mattyblackledge@gmail.com>
2024-03-19 21:51:22 -0500

    Added osirix_utilities tests

---
+++ tests/integration_tests_pyosirix/run_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Create a new virtual environment
+python -m venv test_pyosirix_env
+
+# Activate the virtual environment
+source test_pyosirix_env/bin/activate
+
+# Install the dependencies
+pip install -r requirements.txt
+
+# Clean up previous installations
+pip uninstall -y osirixgrpc pyosirix
+
+# Get the current version
+export PYOSIRIXVERSION=$(cat ../../VERSION)
+
+# Install the package from the test PYPI
+pip install -i https://pypi.org/simple/ --extra-index-url https://test.pypi.org/simple/ pyosirix==$PYOSIRIXVERSION
+
+# Run pytest on the entire test directory: -s flag ensures print statements are available; -x stops after first fail.
+pytest test_osirix_utilities.py -s -x
+
+# Deactivate the virtual environment
+deactivate
+
+# Remove the virtual environment after testing
+rm -rf test_pyosirix_env
--- tests/integration_tests_pyosirix/test_osirix_utilities.py
+++ tests/integration_tests_pyosirix/test_osirix_utilities.py
@@ -25,11 +25,23 @@ def test_viewer_controller_is_closed(global_osirix_instance):
     # There should be no viewer controller open yet.
     with pytest.raises(osirix.exceptions.GrpcException) as exc:
         global_osirix_instance.frontmost_viewer()
-    assert str(exc.value) == "No ViewerController active"
+    assert str(exc.value) == "No ViewerController active", \
+        f"Bad error message for no frontmost viewer controller"
 
 
 def test_vr_controller_is_closed(global_osirix_instance):
     # There should be no vr controller open yet.
     with pytest.raises(osirix.exceptions.GrpcException) as exc:
         global_osirix_instance.frontmost_vr_controller()
-    assert str(exc.value) == "No VRController active"
+    assert str(exc.value) == "No VRController active", \
+        f"Bad error message for no frontmost VR controller"
+
+
+def test_displayed_2d_viewers_is_zero(global_osirix_instance):
+    vcs = global_osirix_instance.displayed_2d_viewers()
+    assert len(vcs) == 0, f"There are one or more ViewerControllers open"
+
+
+def test_displayed_vr_controllers_is_zero(global_osirix_instance):
+    vcs = global_osirix_instance.displayed_vr_controllers()
+    assert len(vcs) == 0, f"There are one or more VRControllers open"
