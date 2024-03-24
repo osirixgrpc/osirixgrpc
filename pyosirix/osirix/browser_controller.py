9d0de63f30223f90f0ba205e9c5286c66c92d621
Matthew Blackledge <mattyblackledge@gmail.com>
2024-03-24 04:24:59 -0500

    Added tests and open viewer for BrowserController pyosirix class

--- pyosirix/osirix/browser_controller.py
+++ pyosirix/osirix/browser_controller.py
@@ -19,6 +19,9 @@ At present this includes operations:
 from __future__ import annotations
 from typing import Tuple, List
 
+import numpy as np
+from numpy.typing import NDArray
+
 import osirixgrpc.browsercontroller_pb2 as browsercontroller_pb2
 
 import osirix
@@ -71,3 +74,92 @@ class BrowserController(osirix.base.OsirixBase):
             selected_series.append(osirix.dicom.DicomSeries(self.osirix_service, series))
 
         return selected_studies, selected_series
+
+    @pyosirix_connection_check
+    def open_viewer_2d(self, dicom_images: NDArray) -> osirix.viewer_controller.ViewerController:
+        """ Open a 2D viewer for an array of osirix.dicom.DicomImage instances.
+
+        Note that images will be loaded in the order in which they are presented as input here.
+        Use the properties of DicomImage to ensure that they are loaded in order (e.g. sorted by
+        the `slice_location` or `instance_number` properties).
+
+        Args:
+            dicom_images (NDArray): The array of images to display. Must be one dimensional and
+                have at least one element.
+
+        Returns:
+            osirix.viewer_controller.ViewerController: The resulting new viewer controller.
+
+        Raises:
+            ValueError: When the number of images in `dicom_images` is less than 1.
+            ValueError: When `dicom_images` is multidimensional.
+            ValueError: When instances other than DicomImage are provided in `dicom_images`.
+        """
+        dicom_images = np.array(dicom_images)
+        if not len(dicom_images) > 0:
+            raise ValueError(f"The number of input images must be one or greater.")
+        if not dicom_images.ndim == 1:
+            raise ValueError(f"The input array must be one dimensional.")
+        for dicom_image in dicom_images:
+            if not isinstance(dicom_image, osirix.dicom.DicomImage):
+                raise ValueError("Only instances of DicomImage are permitted as input.")
+
+        dicom_images = [dicom_image.pb2_object for dicom_image in dicom_images]
+        frames = [browsercontroller_pb2.BrowserControllerOpenViewerFromImagesRequest.FrameImages(
+            images=dicom_images)]
+        request = browsercontroller_pb2.BrowserControllerOpenViewerFromImagesRequest(
+            browser=self.pb2_object, frames=frames, movie=False)
+        response = self.osirix_service_stub.BrowserControllerOpenViewerFromImages(request)
+        self.response_check(response)
+        return osirix.viewer_controller.ViewerController(self.osirix_service, response.viewer)
+
+    @pyosirix_connection_check
+    def open_viewer_4d(self, dicom_images: NDArray) -> osirix.viewer_controller.ViewerController:
+        """ Open a 4D viewer for an array of arrays of osirix.dicom.DicomImage instances.
+
+        As per the `open_viewer_2d` method, each frames will be presented in the order in which
+        the DicomImage instances are presented to the function.
+
+        Note that no pre-checks are performed to ensure OsiriX can actually display the data (e.g.
+        same slice locations are provided for each frame).
+
+        Args:
+            dicom_images (NDArray): An array of shape (N_frames, N_images).
+
+        Returns:
+            osirix.viewer_controller.ViewerController: The resulting new viewer controller.
+
+        Raises:
+            ValueError: When N_frames is less than 2.
+            ValueError: When N_images is less than 1.
+            ValueError: When `dicom_images` shape is not 2-dimensional.
+            ValueError: When instances other than DicomImage are provided in the `dicom_images`.
+        """
+        dicom_images = np.array(dicom_images)
+        if not dicom_images.ndim == 2:
+            raise ValueError(f"The input array must be two dimensional.")
+
+        n_frames, n_images = dicom_images.shape
+        if not n_frames > 1:
+            raise ValueError(f"Number of input frames must be > 1. "
+                             f"For single frame use `open_viewer_2d`.")
+        if not n_images > 0:
+            raise ValueError(f"Number of input images must be 1 or greater.")
+
+        frames = []
+        for i in range(n_frames):
+            frame = []
+            for j in range(n_images):
+                dicom_image = dicom_images[i, j]
+                if not isinstance(dicom_image, osirix.dicom.DicomImage):
+                    raise ValueError("Only instances of DicomImage are permitted as input.")
+                frame.append(dicom_image.pb2_object)
+            frames.append(
+                browsercontroller_pb2.BrowserControllerOpenViewerFromImagesRequest.FrameImages(
+                    images=frame))
+
+        request = browsercontroller_pb2.BrowserControllerOpenViewerFromImagesRequest(
+            browser=self.pb2_object, frames=frames, movie=True)
+        response = self.osirix_service_stub.BrowserControllerOpenViewerFromImages(request)
+        self.response_check(response)
+        return osirix.viewer_controller.ViewerController(self.osirix_service, response.viewer)
--- tests/integration_tests_pyosirix/conftest.py
+++ tests/integration_tests_pyosirix/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 import osirix
@@ -22,3 +23,57 @@ def secondary_service(secondary_port):
     service.start_service()
     yield service
     service.stop_service()
+
+
+@pytest.fixture(scope="function")
+def browser_controller():
+    yield osirix.current_browser()
+
+
+@pytest.fixture(scope="function")
+def dicom_paths():
+    directory = os.path.abspath("../data/MR phantom")
+    paths = [os.path.join(directory, path) for path in os.listdir(directory) if "dcm" in path]
+    yield paths
+
+
+@pytest.fixture(scope="function")
+def study_uid_test():
+    """ The test study UID """
+    yield "1.2.840.846310145.17.1.1.4515953"
+
+
+@pytest.fixture(scope="function")
+def series_uid_test():
+    """ The core test series UID for 2D testing. """
+    yield "1.3.12.2.1107.5.2.18.41171.2022080319011374970780396.0.0.0 TE-98"
+
+
+@pytest.fixture(scope="function")
+def image_uid_test():
+    """ An image SOP instance UID used for testing """
+    yield "1.3.12.2.1107.5.2.18.41171.2022080319050545343380519"
+
+
+@pytest.fixture(scope="function")
+def study_test(browser_controller, study_uid_test):
+    """ Return the DicomStudy used during testing purposes """
+    studies, series = browser_controller.database_selection()
+    test_study = None
+    for study in studies:
+        study_uid = study.study_instance_uid
+        if study_uid == study_uid_test:
+            test_study = study
+    yield test_study
+
+
+@pytest.fixture(scope="function")
+def series_test(study_test, series_uid_test):
+    """ Return the DicomSeries used during testing purposes """
+    series = study_test.series()
+    test_series = None
+    for serie in series:
+        series_uid = serie.series_instance_uid
+        if series_uid == series_uid_test:
+            test_series = serie
+    yield test_series
