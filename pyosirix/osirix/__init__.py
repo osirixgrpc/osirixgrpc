dbf3c8d641170a14268d57d82bc35065098a3546
Matthew Blackledge <mattyblackledge@gmail.com>
2024-06-04 15:02:21 +0100

    bump dev66

--- pyosirix/osirix/__init__.py
+++ pyosirix/osirix/__init__.py
@@ -11,7 +11,7 @@ Typical usage example:
 
 from __future__ import annotations
 
-__version__ = "0.2.1-dev65"
+__version__ = "0.2.1-dev66"
 
 __author__ = "Timothy Sum Hon Mun & Matthew D Blackledge"
 
