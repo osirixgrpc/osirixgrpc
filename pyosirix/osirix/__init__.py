3bb1f29e2575cd7a96155a6669088218213b048c
Matthew Blackledge <mattyblackledge@gmail.com>
2024-06-12 00:56:34 +0100

    bump beta

--- pyosirix/osirix/__init__.py
+++ pyosirix/osirix/__init__.py
@@ -11,7 +11,7 @@ Typical usage example:
 
 from __future__ import annotations
 
-__version__ = "0.2.1-rc0"
+__version__ = "0.2.1-beta0"
 
 __author__ = "Timothy Sum Hon Mun & Matthew D Blackledge"
 
