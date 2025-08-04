327479f4b75a5082b5b4084c8fd2ef7af0dadae4
Matthew Blackledge <matthew.blackledge@icr.ac.uk>
2025-08-04 12:16:26 +0100

    Fixed build script for testing

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the grpc version
-GRPC_VERSION="1.74.0"
+GRPC_VERSION="1.55.0"
 BUILD_PATH="build"
 
 if ! [ -d grpc ]; then
@@ -16,9 +16,9 @@ if ! [ -d grpc ]; then
 
     # Build/install grpc libraries (universal arm64+x86_64)
     # ====================================================
-    mkdir -p cmake/build-universal
-    mkdir -p install-universal
-    pushd cmake/build-universal
+    mkdir -p cmake/build
+    mkdir -p install
+    pushd cmake/build
 
     cmake -DgRPC_INSTALL=ON \
           -DgRPC_BUILD_TESTS=OFF \
@@ -26,6 +26,7 @@ if ! [ -d grpc ]; then
           -DCMAKE_INSTALL_PREFIX=../../install-universal \
           -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
           -DgRPC_CARES_PROVIDER=package \
+          -DCMAKE_POLICY_VERSION_MINIMUM=3.5\
           ../..
 
     make -j 4
@@ -33,33 +34,33 @@ if ! [ -d grpc ]; then
     popd
 
     # Copy binaries/headers
-    cp -r install-universal/bin install
-    cp -r install-universal/include install
-    cp -r install-universal/lib install
+    cp -r install/bin install
+    cp -r install/include install
+    cp -r install/lib install
 
     popd
 fi
 
 # Build the cpp definitions
 mkdir -p cpp
-grpc/install-universal/bin/protoc -I protos --grpc_out=cpp --plugin=protoc-gen-grpc=grpc/install-universal/bin/grpc_cpp_plugin osirix.proto
-grpc/install-universal/bin/protoc -I protos --cpp_out=cpp osirix.proto roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
+grpc/install/bin/protoc -I protos --grpc_out=cpp --plugin=protoc-gen-grpc=grpc/install/bin/grpc_cpp_plugin osirix.proto
+grpc/install/bin/protoc -I protos --cpp_out=cpp osirix.proto roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
 
 # Build the Python definitions
 mkdir -p python/osirixgrpc
-grpc/install-universal/bin/protoc -I protos --grpc_python_out=python/osirixgrpc --plugin=protoc-gen-grpc_python=grpc/install-universal/bin/grpc_python_plugin osirix.proto
-grpc/install-universal/bin/protoc -I protos --python_out=python/osirixgrpc osirix.proto roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
+grpc/install/bin/protoc -I protos --grpc_python_out=python/osirixgrpc --plugin=protoc-gen-grpc_python=grpc/install/bin/grpc_python_plugin osirix.proto
+grpc/install/bin/protoc -I protos --python_out=python/osirixgrpc osirix.proto roi.proto roivolume.proto dcmpix.proto vrcontroller.proto viewercontroller.proto browsercontroller.proto dicomimage.proto dicomstudy.proto dicomseries.proto utilities.proto types.proto
 
 # Update the requirements file
-echo "grpcio>=$GRPC_VERSION" > python/requirements.txt
-echo "grpcio-tools>=$GRPC_VERSION" >> python/requirements.txt
+#echo "grpcio>=$GRPC_VERSION" > python/requirements.txt
+#echo "grpcio-tools>=$GRPC_VERSION" >> python/requirements.txt
 
 # Build project
-mkdir -p src/"$BUILD_PATH"
-xcodebuild -project src/osirixgrpc.xcodeproj -scheme osirixgrpc_build -destination "generic/platform=macOS" CONFIGURATION_BUILD_DIR="$BUILD_PATH" build
+#mkdir -p src/"$BUILD_PATH"
+#xcodebuild -project src/osirixgrpc.xcodeproj -scheme osirixgrpc_build -destination "generic/platform=macOS" CONFIGURATION_BUILD_DIR="$BUILD_PATH" build
 
 # Zip plugin
-cd src/$BUILD_PATH
-zip -r osirixgrpc.osirixplugin.zip osirixgrpc.osirixplugin
+#cd src/$BUILD_PATH
+#zip -r osirixgrpc.osirixplugin.zip osirixgrpc.osirixplugin
 
 echo "Finished build!"
